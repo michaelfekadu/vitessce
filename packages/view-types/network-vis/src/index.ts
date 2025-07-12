c0abc0a7eacba0f6d3b561d35d777f06b13af96a
Eric Moerth <ericmoerth@g.harvard.edu>
2025-07-11 23:49:51 -0400

    Fixing the network index file once more

--- packages/view-types/network-vis/src/index.ts
+++ packages/view-types/network-vis/src/index.ts
@@ -1,4 +1,1 @@
 export { NetworkVisSubscriber } from './NetworkVisSubscriber.js';
-export { NetworkVis } from './NetworkVis.js';
-export { CytoscapeWrapper } from './components/CytoscapeWrapper.js';
-export { MotifSketch } from './components/MotifSketch.js';
